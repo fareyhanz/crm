4ebc496b80161e6464c7a72a2c64a16fb6cd4a43
fareyhanz <uchiha.reyhan@gmail.com>
2016-05-11 16:13:31 +0700

    penambahan modal di activity

--- resources/views/admin/activities/index.blade.php
+++ resources/views/admin/activities/index.blade.php
@@ -11,7 +11,7 @@
 @section('body')
   <div class="box">
     <div class="box-body">
-      <button class="btn btn-primary">Tambah Activity</button>
+      <button class="btn btn-primary" data-toggle="modal" data-target="#modal-new-oportunity">Tambah Activity</button>
 
       <table class="table">
         <thead>
@@ -64,6 +64,48 @@
           @endfor
         </body>
       </table>
+    </div>
+  </div>
+@endsection
+
+@section('modal')
+  <div id="modal-new-oportunity" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Activity</h4>
+        </div>
+        <div class="modal-body">
+          <form class="form" action="" method="get">
+            <div class="form-group">
+              <label>Activiity</label>
+              <input class="form-control" type="text" name="activity" placeholder="Text">
+            </div>
+            <div class="form-group">
+              <label>Tempat</label>
+              <input class="form-control" type="text" name="tempat" placeholder="Text">
+            </div>
+            <div class="form-group">
+              <label>Tanggal</label>
+              <input type="date" class="form-control" name="">
+            </div>
+            <div class="form-group">
+              <label>Deskripsi</label>
+              <input class="form-control" type="text" name="amount" placeholder="Text">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <div class="btn-group">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Simpan</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 @endsection
